cf2ad6c589f837be7b72c3877a1b38fcfe582180
Sunil Kumar Kashyap <147919215+kumarsunilkashyap@users.noreply.github.com>
2025-01-27 06:16:41 +0000

    Update gallery

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = () => {
     const fetchPhotos = async () => {
       try {
         const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/albums",
+          "https://jsonplaceholder.typicode.com/photos",
           {
             headers: {
               Authorization: `Client-ID YOUR_ACCESS_KEY`,
@@ -25,9 +25,9 @@ const Gallery = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Photo Gallery</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="min-h-screen p-20 container mx-auto">
+      <h1 className="text-3xl font-bold text-center p-10">Photo Gallery</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-5 gap-4">
         {photos.map((photo) => (
           <div key={photo.id} className="relative">
             <img
@@ -35,8 +35,8 @@ const Gallery = () => {
               alt={photo.alt_description}
               className="w-full h-auto rounded shadow-lg"
             />
-            <div className="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 rounded-b w-full text-center">
-              {photo.title}
+            <div className="absolute bottom-0 left-0 bg-gray-500 bg-opacity-50 text-white p-2 rounded-b w-full text-center">
+              {photo.url}
             </div>
           </div>
         ))}
